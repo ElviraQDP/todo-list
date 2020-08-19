5e97e94bd8d6f09f94198d1b879d5cfb502f6050
Elvira Quiroz <elvira.quiroz.dp@gmail.com>
2020-08-19 23:01:37 +0000

    delete incluido

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,43 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom'
 import './index.css';
 import Todos from './todos';
-import AddTodo from './addform'
 
-class App extends Component {
-    state = {
-        todos: [
-            {id: 1, content: 'buy a pair of jeans'},
-            {id: 2, content: 'play with my niece'},
-        ]
-    }
 
-    deleteTodo = (id) => {
-        console.log("delete")
-        const todos = this.state.todos.filter(todo => {
-            return todo.id !== id
-        });
 
-        this.setState({
-            todos
-        });
-    }
-    addTodo = (todo) => {
-        todo.id = Math.random()
-        let todos = [...this.state.todos, todo];
-        this.setState ({
-            todos 
-        })
-    }
-  render() {
-    return (
-      <div className="todo-app container">
-        <h1 className="center blue-text">Todos</h1>
-            <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-            <AddTodo addTodo={this.addTodo}/>
-      </div>
-    );
-  }
-}
+
   
-ReactDom.render(<App/>, document.querySelector("#root"));
+ReactDom.render(<Todos/>, document.querySelector("#root"));
--- todo-list/src/todos.js
+++ todo-list/src/todos.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Todos = ({todos, deleteTodo}) => {
-    const todoList = todos.length ? (
-        todos.map(todo => {
-            console.log(todo);
-            return (
-                <div className="collection-item" key={todo.id}>
-                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
-                </div>
-            )
-        })
-    ) : (
-        <p className="noTodos">go ahead and get some free time!</p>
+const Todos = () => {
+    const [todos, setTodos] = useState(
+        []
     )
+    const handleChange = (e) => {
+        if (e.keyCode === 13) {   // capturamos el valor del todo cuando presionamos la tecla "enter"
+            e.preventDefault()    // evitamos que la pagina se recargue ante el el evento "onChange"
+            setTodos([...todos, e.target.value]) // definimos queel todo capturado anteriormente se añada a un array de "todos"
+            e.target.value = "" // esta linea boora el input una vez que sido capturado 
+        }
+    }
+    const handleClick = (i) => { // aqui capturamos el evento onChange
+        const newTodos = [...todos] // clonar el array original
+        newTodos.splice(i,1) // eliminar el elemento del array
+        setTodos([...newTodos])
+
+
+    }
+    console.log(todos)
     return (
-        <div className="todoList">
-        {todoList}
-        </div>
+        <> 
+            <h1 class="text-align:center">Todo's</h1>
+            <form>
+                <input type="text" onKeyDown={(e) => handleChange(e)} /> 
+            </form> 
+            <ul>
+                { 
+                    todos.map((todo, i) => { /// recorre el array todos y añade cada todo a un li
+                        return (
+                            <li key={i} onClick={() => handleClick(i)}>{todo}</li>
+                        )
+                    })
+                }
+            </ul>
+        </>
+
     )
+
 }
 
 export default Todos;
